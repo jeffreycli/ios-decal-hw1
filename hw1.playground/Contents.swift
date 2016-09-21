5ad301b15bafebb6ffa8da9b940c75a63a4195fb
Jeffrey Li <jeffreyli@MacBook-Pro.local>
2016-09-20 23:45:30 -0700

    I commented on several lines and still can't find the bug. This is just a checkpoint commit.

--- hw1.playground/Contents.swift
+++ hw1.playground/Contents.swift
@@ -21,67 +21,69 @@ class Words {
 //: ### Are the values passed in to the **init** function and those set to the instance
 //: ### variables the same type? If not, why?
 
+//: The values passed into the init function and the values set to the instance variables possible will be the of the same type because the parameter passed into the init function is used to assign the instance variable.
 
-//: [EXPLAIN YOUR ANSWER HERE]
 
-
-//: ## Q2: Variable Types and Function Types
-    func arePalindromes(_ words: [String]) -> Bool {
-        let reversedWords = words.map() {String($0.characters.reversed())}
-        let numElements = words.count
+    //: ## Q2: Variable Types and Function Types
+    func arePalindromes(_ words: [String]) -> Bool { //func arePalindromes takes in string array labeled words and returns a boolean
+        let reversedWords = words.map() {String($0.characters.reversed())} //reverses the string argument
+        let numElements = words.count //counts the numbers of letters in the word
         
-        for i in 0 ..< numElements {
+        for i in 0 ..< numElements { //iterates over the index of the string to see if letters are the same when word is reversed
             if words[i] != reversedWords[i] {
                 return false
+            } else { //I added this line in because there is no return True line
+                return true
             }
         }
     }
-//: ### Why does the compiler dislike the **for loop**? Fix it.
-//: ### What else is wrong with this function? You may have to refer to (but **not**
-//: ### change) the code at the very bottom. Debug the function.
-
-
-//: [EXPLAIN YOUR ANSWER HERE]
-
-
-//: ## Q3: More Functions and Object Initialization
+    //: ### Why does the compiler dislike the **for loop**? Fix it.
+    //: ### What else is wrong with this function? You may have to refer to (but **not**
+    //: ### change) the code at the very bottom. Debug the function.
+    
+    
+    //: [I think this function didn't have a return true statatement because at the bottom, there are palindromes that get passed into this function.]
+    
+    
+    //: ## Q3: More Functions and Object Initialization
     class func isAnagram() -> Bool {
-        var countLetters : [Character : Int] //Line X
-        var lenA = self.wordA.characters.count
-        var lenB = self.wordB.characters.count
+        //var countLetters : [Character : Int] //Line X
+        var countLetters = [Character : Int]() //Line X, creates empty dictionary of keys character and value integer
+        var lenA = wordA.characters.count //gets lengths of wordA and wordB
+        var lenB = wordB.characters.count
         
-        if lenA != lenB {
+        if lenA != lenB { //compares length of wordA and wordB. If they aren't equal, then they can't be anagrams of each other
             return false
         }
         
         var arrA = Array(self.wordA.characters)
         var arrB = Array(self.wordB.characters)
         
-        for i in 0...lenA-1 {
+        for i in 0...lenA-1 { //iterates ever character in arrA and counts the appearance of each letter into a dictionary
             let letter = arrA[i]
-            if let val = countLetters[letter] { //Line Y
+            if let val = countLetters[letter] { //Line Y, letter is already in dictionary so value gets updated
                 countLetters[letter] = val + 1
-            } else {
+            } else { //new letter in dictionary so value is set to 1
                 countLetters[letter] = 1
             }
         }
         
         for i in 0...lenB-1 {
             let letter = arrB[i]
-            if let val = countLetters[letter] {
+            if let val = countLetters[letter] { //decrements the value by one everytime the letter shows up in the word for the dict
                 countLetters[letter] = val - 1
-            } else {
+            } else { //if new letter appears, the words are not anagrams of each other
                 return false
             }
         }
         
-        for (letter, count) in countLetters {
+        for (letter, count) in countLetters { //iterates through values in the dictionary to see if there are any leftover letters or extra letters when arrB decremented the value by the letter of the key
             if count != 0 {
                 return false
             }
         }
         
-        return nil
+        return true
     }
 //: ### What is the problem with declaring **countLetters** as we do in **Line X**,
 //: ### and then using it in **Line Y**? Fix it (by only changing **Line X**).
@@ -89,7 +91,7 @@ class Words {
 //: ### change) the code at the very bottom. Debug the function.
 
 
-//: [EXPLAIN YOUR ANSWER HERE]
+//: ### [In Line X, the convention for creating the empty dictionary would be to use an equal sign for assignment and using paranthesis after the braket to create the empty dictionary. ]
     
     
 }
