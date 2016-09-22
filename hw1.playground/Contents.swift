43a1331a9bc7e7cd6ec624562f396e9197fea4a0
Jeffrey Li <jeffreyli@MacBook-Pro.local>
2016-09-21 19:28:19 -0700

    Finished homework

--- hw1.playground/Contents.swift
+++ hw1.playground/Contents.swift
@@ -17,40 +17,42 @@ class Words {
         self.wordA = wordA
         self.wordB = wordB
     }
-
+    
 //: ### Are the values passed in to the **init** function and those set to the instance
 //: ### variables the same type? If not, why?
-
-//: The values passed into the init function and the values set to the instance variables possible will be the of the same type because the parameter passed into the init function is used to assign the instance variable.
-
-
-    //: ## Q2: Variable Types and Function Types
-    func arePalindromes(_ words: [String]) -> Bool { //func arePalindromes takes in string array labeled words and returns a boolean
+    
+//: [The values passed into the init function and the values set to the instance variables possible will be the of the same type because the parameter passed into the init function is used to assign the instance variable.]
+    
+    
+//: ## Q2: Variable Types and Function Types
+    class func arePalindromes(_ words: [String]) -> Bool { //func arePalindromes takes in string array labeled words and returns a boolean
+        // This is a static function because at the bottom, this function is called on an array of its own so it doesn't need an instance to be called upon it. Further, it is its own class to make it static. Salil explained to me.
         let reversedWords = words.map() {String($0.characters.reversed())} //reverses the string argument
         let numElements = words.count //counts the numbers of letters in the word
         
         for i in 0 ..< numElements { //iterates over the index of the string to see if letters are the same when word is reversed
             if words[i] != reversedWords[i] {
                 return false
-            } else { //I added this line in because there is no return True line
-                return true
             }
         }
+        
+        return true
     }
-    //: ### Why does the compiler dislike the **for loop**? Fix it.
-    //: ### What else is wrong with this function? You may have to refer to (but **not**
-    //: ### change) the code at the very bottom. Debug the function.
+//: ### Why does the compiler dislike the **for loop**? Fix it.
+//: ### What else is wrong with this function? You may have to refer to (but **not**
+//: ### change) the code at the very bottom. Debug the function.
     
     
-    //: [I think this function didn't have a return true statatement because at the bottom, there are palindromes that get passed into this function.]
+//: [This function didn't have a return true statatement because at the bottom, there are palindromes that get passed into this function.]
     
     
-    //: ## Q3: More Functions and Object Initialization
-    class func isAnagram() -> Bool {
+//: ## Q3: More Functions and Object Initialization
+    func isAnagram() -> Bool {
+        // For this function, the code at the bottom is used on an instance of the class Words object, wordsObj. This function is not static because it is used on an instance of the class Words so you need to make an instance of the class you want call this method on before you can use it to check anagrmas.
         //var countLetters : [Character : Int] //Line X
         var countLetters = [Character : Int]() //Line X, creates empty dictionary of keys character and value integer
-        var lenA = wordA.characters.count //gets lengths of wordA and wordB
-        var lenB = wordB.characters.count
+        var lenA = self.wordA.characters.count //gets lengths of wordA and wordB
+        var lenB = self.wordB.characters.count
         
         if lenA != lenB { //compares length of wordA and wordB. If they aren't equal, then they can't be anagrams of each other
             return false
@@ -89,9 +91,9 @@ class Words {
 //: ### and then using it in **Line Y**? Fix it (by only changing **Line X**).
 //: ### What else is wrong with this function? You may have to refer to (but **not**
 //: ### change) the code at the very bottom. Debug the function.
-
-
-//: ### [In Line X, the convention for creating the empty dictionary would be to use an equal sign for assignment and using paranthesis after the braket to create the empty dictionary. ]
+    
+    
+//: [In Line X, the convention for creating the empty dictionary would be to use an equal sign for assignment and using paranthesis after the braket to create the empty dictionary. At the bottom, isAnagram() is called on an instance of Words, wordObj. For this to work, it can't have "class" in front of the function because the code at the bottom uses this method on an instance of Words. I go into further detail in my comments at line 29 and 51]
     
     
 }
